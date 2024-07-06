5324551ef94c63e5a233f5db7355de0343ca3e42
elishakstevens <elisha.stevens@gmail.com>
2024-07-06 15:33:45 -0700

    Updating JS

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,25 @@
+var str = "Hello, World!!"; //Assigning str variable
+window.alert(str); //Alert box displaying str
+document.write("Hello, World!!!"); //Display text in DOM
+
+var string = "This is a string"; //Asign string variable
+document.write(string); //Display string in DOM
+
+var string2 = "We\'re not going to go Grandma\'s house today. We\'ll be going next week." //Assign string2 variable - Using backslashes for apostrophes in text
+document.write(string2); //Display string2 in DOM
+
+var Sent1 = "This is the beginning of the string"; //Assign Sent1 variable
+var Sent2 = " and this is the end of the string"; //Assign Sent2 variable
+document.write(Sent1 + Sent2); //Concatenate Sent1 and Sent2 and display on DOM
+
+document.write("I enjoy listening to Dark and Indie Pop, " 
+    + "but I also love to listen to artists like Sabrina Carpenter and Taylor Swift."
+    + " My favorite song right now is Good Luck, Babe by Chappell Roan!");  //Display concatenated string in DOM
+
+var string3 = "Concatenated " + "String"; //Assigning string3 variable with conatenated string
+document.write(string3); //Display string3 in DOM
+
+var fruit = "apple", vegetable = "spinach", meat = "chicken", dairy = "yogurt", snack = "chips", dessert = "cheesecake"; //Assigning multiple variables
+document.write(snack); //Display snack in DOM
+
+document.write(52 + 43); //Display sum of numbers in DOM
